3de2e45343222a6186f76abe10c63cb19779c757
Hm7420hn <7mo0ody7m105690@gmail.com>
2024-08-06 21:57:57 +0700

    nodejs mysql get data

--- app-database.js
+++ app-database.js
@@ -1,2 +1,32 @@
-const http   = require('http') //model bawaan dari node.js
-const mysql2 = require('mysql2') //modul dari node_modules
+const { log } = require('console')
+const http    = require('http') //model bawaan dari node.js
+const mysql  = require('mysql2') //modul dari node_modules
+
+// konfigurasi database mysql yg ingin digunakan
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jfd_belajar_database',
+}) 
+
+// menyambungkan atau membuka koneksi
+db.connect()
+
+// ambil data dari mysql
+db.query("SELECT * FROM karyawan", (error, hasil)=> {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(hasil);   
+        console.log('==========');   
+        console.log(hasil[1].nama);
+        console.log('=================');
+        for (let i = 0; i < hasil.length; i++) {
+            console.log(hasil[i].nama);
+            
+        }      
+    }
+})
+
+db.end()
