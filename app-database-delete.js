899c49ee77a3fd36841d88d804674c1b0598995a
Hm7420hn <7mo0ody7m105690@gmail.com>
2024-08-08 20:03:57 +0700

    nodejs mysql delete

---
+++ app-database-delete.js
@@ -0,0 +1,33 @@
+const { log } = require('console')
+const http    = require('http') //model bawaan dari node.js
+const mysql  = require('mysql2') //modul dari node_modules
+
+// konfigurasi database mysql yg ingin digunakan
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jfd_belajar_database',
+}) 
+
+// menyambungkan atau membuka koneksi
+db.connect()
+
+
+let sql =
+`DELETE FROM karyawan 
+WHERE id = 14`
+
+// memasukkan/menembahan data dari mysql
+db.query( sql, (error, hasil)=> {
+    if (error) {
+        console.log(error);
+    } else {
+        // console.log(hasil);
+        if (hasil.affectedRows > 0) {
+            console.log('berhasil insert data karyawan');
+        }   
+    }
+})
+
+db.end()
