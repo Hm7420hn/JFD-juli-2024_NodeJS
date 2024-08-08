402083ddbdfedce1c41f13903023f19aeb99036f
Hm7420hn <7mo0ody7m105690@gmail.com>
2024-08-08 22:01:42 +0700

    query string di url

--- app4.js
+++ app4.js
@@ -1,5 +1,7 @@
 const http = require('http')
 const fs = require('fs')
+const URL = require('url')
+const qs = require('querystring')
 const mysql = require('mysql2')
 
 
@@ -14,6 +16,24 @@ db.connect()
 
 
 let server = http.createServer(function(request,respon) {
+
+    // console.log(qs.parse(URL.parse(request.url)))
+    let qstring = qs.parse(URL.parse(request.url).query)
+    console.log(qstring.nama);
+    if (qstring.nama) {
+        db.query(`SELECT * FROM karyawan WHERE nama = ?`, [qstring.nama],(error, hasil)=>{
+            if (error) {
+                console.log(error);
+            } else {
+                return respon.write(
+                    `<pr>
+                    ${JSON.stringify(hasil)}
+                    </pr>`
+                )
+            }
+        } )
+    }
+
     if (request.url == '/') {
         respon.writeHead(200, {'Content-type': 'text/html'})
         fs.createReadStream('./view/beranda.html').pipe(respon)
@@ -25,15 +45,14 @@ let server = http.createServer(function(request,respon) {
             if (error) {
                 console.log(error);
             } else {
-                // buat variable kososng 
+                // buat variable kosong 
                 let datakry = ''
                 for (let i = 0; i < hasil.length; i++) {
                     // isi variable kosong dengan loopingan data dari db
                     datakry += hasil[i].nama + ' - ' + hasil[i].gender + ' - ' + hasil[i].alamat + ' - ' + hasil[i].nip + '<br>'
                 }      
                 // kirim ke frontend menggunakan respon manual 
-                // karena fs.creatReadStream tidak mampu
-                // menerima kiriman data dari backend
+                // karena fs.creatReadStream tidak mampu menerima kiriman data dari backend
                 respon.write(
                     `<h1>Berita terupdate dari kami:</h1><hr>
                     ${datakry}`
