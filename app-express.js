fc1bea60dff1fc4e36ec3b62c67e40c06cfae68b
Hm7420hn <7mo0ody7m105690@gmail.com>
2024-08-09 21:59:41 +0700

    nodejs express

--- app-express.js
+++ app-express.js
@@ -2,6 +2,10 @@ const express   = require('express')
 const app       = express()
 const port = 3000
 
+app.set('view engine', 'ejs')   //setting penggunaan template engine untuk express
+app.set('views', './view-ejs')   //setting penggunaan folder untuk menyempan file .ejs
+
+
 
 app.get('/', (req, res)=>{
     res.send('Hello World!!')
@@ -9,7 +13,23 @@ app.get('/', (req, res)=>{
 
 // route -> rute
 app.get('/hubungi', function(req, res) {
-    res.send('<h1>Silahkan WA Saya: 081380175488</h1>')
+    let data = {
+        Email: 'moodyhamoody74@gmail.com',
+        Instagram: '7amoody7mh'
+    }
+    res.render('hubungi-developer', data)
+    // res.send('<h1>Silahkan WA Saya: 081380175488</h1>')
+})
+
+app.get('/profil', (req, res)=> {
+    let data = {
+        jabatan: 'Senior Programmer',
+        gender:  'Laki',
+        gaji: 8000000
+    }
+    res.render('profil-developer', data)
+    // error, karena express tidak bisa membaca file dengan extensi . html
+    // res.send(require('./view/profil.html'))  
 })
 
 app.listen(port,()=> {
